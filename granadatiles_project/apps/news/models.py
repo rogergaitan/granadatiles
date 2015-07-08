07a70d7a8958f56916243508b7b429e76d1cdcdc
Wuelber Castillo <wuelber.castillo@gmail.com>
2015-07-08 14:14:50 -0600

    Create the Magazine and Video models. Add the attributes and translation for every model.

--- granadatiles_project/apps/news/models.py
+++ granadatiles_project/apps/news/models.py
@@ -1,6 +1,37 @@
-from apps.utils import BaseModel
+from apps.utils import BaseModel, BaseDescriptionImageModel
 from apps.utils.methods import model_directory_path
+from django.utils.translation import ugettext as _
 
 
 class Catalog(BaseModel):
-    file = FileField(upload_to=model_directory_path)
+    file = FileField(upload_to=model_directory_path, verbose_name=_('File'))
+
+    class Meta:
+        verbose_name = _('Catalog')
+        verbose_name_pluraal = _('Catalogs')
+
+    def __str__(self):
+        return self.title
+
+
+class Magazine(BaseDescriptionImageModel):
+    date = DateField(verbose_name=_('Date'))
+
+    class Meta:
+        verbose_name = _('Magazine')
+        verbose_name_pluraal = _('Magazines')
+
+
+    def __str__(self):
+        return self.title
+
+
+class Video(BaseModel):
+    url = CharField(max_length=11, verbose_name=_('URL'))
+
+    class Meta:
+        verbose_name = _('Video')
+        verbose_name_pluraal = _('Videos')
+
+    def __str__(self):
+        return self.title
