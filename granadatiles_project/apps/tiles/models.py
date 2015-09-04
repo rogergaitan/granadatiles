81896c4cc301da4fdec8642e63e19b58e7a50159
roger <roger@localhost.localdomain>
2015-09-04 14:07:42 -0600

    add tile section group relation

--- granadatiles_project/apps/tiles/models.py
+++ granadatiles_project/apps/tiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.utils.models import BaseDescriptionImageModel
+from apps.content.model import SectionImage
 
 class Collection(BaseDescriptionImageModel):
   pass
@@ -9,3 +10,4 @@ class Group(BaseDescriptionImageModel):
   
 class Tile(BaseDescriptionImageModel):
   group = models.ForeignKey(Group)
+  section_image = models.ForeignKey(SectionImage)
