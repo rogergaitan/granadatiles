c8bf4fb6b6f696c888d46baa832b8e35d6c8f010
roger <roger@localhost.localdomain>
2015-09-04 11:33:26 -0600

    add tiles admin

--- granadatiles_project/apps/tiles/admin.py
+++ granadatiles_project/apps/tiles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Tile, Collection, Group
 
-# Register your models here.
+class TileAdmin(admin.ModelAdmin):
+    pass
+
+class CollectionAdmin(admin.ModelAdmin):
+    pass
+
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Tile, TileAdmin)
+admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Group, GroupAdmin)
