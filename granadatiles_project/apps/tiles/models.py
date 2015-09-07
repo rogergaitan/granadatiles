66834fa46da9a539a0774df58df88df6fd2915b3
roger <roger@localhost.localdomain>
2015-09-07 09:55:41 -0600

    update tiles models

--- granadatiles_project/apps/tiles/models.py
+++ granadatiles_project/apps/tiles/models.py
@@ -1,25 +1,44 @@
 ﻿from django.db import models
+from django.utils.translation import ugettext as _
 from core.models import BaseGallerieImageModel, BaseCatalogModel, BaseContentModel
 
 
 class TileSize(models.Model):
-    weight = models.IntegerField()
-    thickness = models.IntegerField()
-
-
+    weight = models.IntegerField(verbose_name=_('Weight'))
+    thickness = models.IntegerField(verbose_name=_('Thickness'))
+    
+    class Meta:
+		verbose_name = _('TileSize')
+		verbose_name_plural = _('TileSizes')
+	
+	
 class PalleteColor(BaseCatalogModel):
-    hexadecimalCode = models.CharField(max_length=20)
+    hexadecimalCode = models.CharField(max_length=20, verbose_name=_('Color'))
+    
+    class Meta:
+		verbose_name = _('PalleteColor')
+		verbose_name_plural = _('PalleteColors')
 
 
 class Collection(BaseGallerieImageModel):
-    pass
+    
+    class Meta:
+		verbose_name = _('Collection')
+		verbose_name_plural = _('Collections')
 
 
 class Group(BaseGallerieImageModel):
-    collection = models.ForeignKey(Collection)
+    collection = models.ForeignKey(Collection, related_name='groups', verbose_name=_('Collection'))
 
+     class Meta:
+        verbose_name = _('Group')
+        verbose_name_plural = _('Groups')
 
 class Tile(BaseContentModel):
-    group = models.ForeignKey(Group)
-    sizes = models.ManyToManyField(TileSize)
-    colors = models.ManyToManyField(PalleteColor)
+    group = models.ForeignKey(Group, related_name='tiles', verbose_name=_('Tiles Group'))
+    sizes = models.ManyToManyField(TileSize, related_name='tiles', verbose_name=_('Tiles Sizes'))
+    colors = models.ManyToManyField(PalleteColor, related_name='tiles', verbose_name=_('Tiles Colors'))
+    
+     class Meta:
+        verbose_name = _('Tile')
+        verbose_name_plural = _('Tiles')
