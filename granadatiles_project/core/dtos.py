4435cf8e46de34c70ca806d4e9ecfec12d6fce67
LRPalacios <luisz54@hotmail.com>
2015-09-15 12:34:26 -0600

    update parameter name

--- granadatiles_project/core/dtos.py
+++ granadatiles_project/core/dtos.py
@@ -1,55 +1,55 @@
 ﻿class BaseDto(object):
-
-    def __init__(self, base):
-        self.id = base.id
+    
+    def __init__(self, model):
+        self.id = model.id
 
 
 class BaseCatalogDto(BaseDto):
 
-    def __init__(self, baseCatalog, language=None):
-        super().__init__(baseOrderCatalog)
+    def __init__(self, baseCatalogModel, language=None):
+        super().__init__(baseCatalogModel)
         if language:
-            self.name = baseCatalog.get_name(language)
+            self.name = baseCatalogModel.get_name(language)
         else:
-            self.name = baseCatalog.name
+            self.name = baseCatalogModel.name
 
 
 class BaseCatalogOrderDto(BaseCatalogDto):
 
-    def __init__(self, baseOrderCatalog, language=None):
-        super().__init__(baseOrderCatalog, language)
-        self.order = baseOrderCatalog.order
+    def __init__(self, baseOrderCatalogModel, language=None):
+        super().__init__(baseOrderCatalogModel, language)
+        self.order = baseOrderCatalogModel.order
 
 
 class BaseContentDto(BaseDto):
 
-    def __init__(self, baseContent, language=None):
-        super().__init__(baseContent)
+    def __init__(self, baseContentModel, language=None):
+        super().__init__(baseContentModel)
         if language:
-            self.title = baseContent.get_title(language)
-            self.description = baseContent.get_description(language)
+            self.title = baseContentModel.get_title(language)
+            self.description = baseContentModel.get_description(language)
         else:
-            self.title = baseContent.title
-            self.description = baseContent.description
+            self.title = baseContentModel.title
+            self.description = baseContentModel.description
 
 
 class BaseContentOrderDto(BaseContentDto):
 
-    def __init__(self, baseContentOrder, language=None):
-        super().__init__(baseContentOrder, language)
-        self.order = baseContentOrder.order
+    def __init__(self, baseContentOrderModel, language=None):
+        super().__init__(baseContentOrderModel, language)
+        self.order = baseContentOrderModel.order
 
 
 class BaseGalleryImageDto(BaseContentDto):
 
-    def __init__(self, baseGalleryImage, language=None):
-        self.image = baseGalleryImage.image.url if baseGalleryImage.image else ''
-        super().__init__(baseGalleryImage, language)
+    def __init__(self, baseGalleryImageModel, language=None):
+        self.image = baseGalleryImageModel.image.url if baseGalleryImageModel.image else ''
+        super().__init__(baseGalleryImageModel, language)
 
 
 class BaseGallerieNavImageModel(BaseGalleryImageDto):
 
-    def __init__(self, baseGalleryNavImage, language=None):
-        super().__init__(baseGalleryNavImage, language)
-        self.target = baseGalleryNavImage.target
-        self.link = baseGalleryNavImage.link
+    def __init__(self, baseGalleryNavImageModel, language=None):
+        super().__init__(baseGalleryNavImageModel, language)
+        self.target = baseGalleryNavImageModel.target
+        self.link = baseGalleryNavImageModel.link
