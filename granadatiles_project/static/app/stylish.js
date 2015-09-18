2ecda58f6aa058bda6654b22f01a3ae75f14ed14
LRPalacios <lrpalaciosdev@gmail.com>
2015-09-18 15:24:52 -0600

    add stylish animation

---
+++ granadatiles_project/static/app/stylish.js
@@ -0,0 +1,17 @@
+﻿/* =================================
+   LOADER                     
+=================================== */
+// makes sure the whole site is loaded
+jQuery(window).load(function () {
+    // will first fade out the loading animation
+    jQuery(".status").fadeOut();
+    // will fade out the whole DIV that covers the website.
+    jQuery(".preloader").delay(500).fadeOut("slow");
+})
+
+
+/* =================================
+===  WOW ANIMATION             ====
+=================================== */
+
+new WOW().init();
