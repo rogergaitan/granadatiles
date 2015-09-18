b8e6e3a7ce0a0d3bea9fee12421a5ba1ae1346fc
Vanessa_Lacayo <vanessitalacayo@gmail.com>
2015-09-18 13:20:57 -0600

    Add url button view collection, style home

--- granadatiles_project/static/app/content/services/areaSvc.js
+++ granadatiles_project/static/app/content/services/areaSvc.js
@@ -20,7 +20,7 @@
         //TODO API /api/area/:id
         function getMockArea() {
             return {
-                description: '<h1>Get inspired by Residential and Commercial installation photos</h1><h3>then choose from in stock and custom tiles.</h3>',
+                description: '<h2 class="heading-sec">Get inspired by Residential and Commercial installation photos</h2><h3 class="slogan">then choose from in stock and custom tiles.</h3>',
                 id: 1
             };
         }
