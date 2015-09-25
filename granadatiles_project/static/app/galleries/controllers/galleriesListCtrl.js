2a6f73fab31e4370678b9b64e5be359b248bff50
Erving Bonilla <ervingjbonilla@gmail.com>
2015-09-25 11:50:56 -0600

    correction in bind html with angular js, and add class css

--- granadatiles_project/static/app/galleries/controllers/galleriesListCtrl.js
+++ granadatiles_project/static/app/galleries/controllers/galleriesListCtrl.js
@@ -13,15 +13,17 @@
 
         vm.title = 'Photos of Cement & Concrete Tile Installations';
 
-        vm.description = 'Granada Tile is pleased to share photos of some of the cement tile installations using our ﬂagship\ ' +
+        vm.breadcums = 'Gallery »';
+
+        vm.description = '<p>Granada Tile is pleased to share photos of some of the cement tile installations using our ﬂagship\ ' +
             'Echo Collection tiles. Our hand made cement and concrete tiles have been used in private residences and commercial\ ' +
             'projects, indoors and outdoors, and on ﬂoors and on walls (and even on ceilings). These cement and concrete tile\ ' +
             'installation photos feature bathroom tile, kitchen tile, tile backslashes, wall tiles, ﬂoor tiles, commercial tiles,\ ' +
-            'restaurant tiles, spa tiles, and patio tiles.' + '<br>'+ 'Depending on the setting, some of these hand made cement tiles\ ' +
+            'restaurant tiles, spa tiles, and patio tiles.</p> <p>Depending on the setting, some of these hand made cement tiles\ ' +
             'evoke French and Moroccan tiles, such as the Bouchon Bistro in Beverly Hills and the Fez entryway to a Moroccan-style\ ' +
             'private residence. Other tile installations attain a chic modern esthetic, like the Delphine Restaurant in the W. Hotel\ ' +
-            'and the Serengeti tile at the Terranea Resort and Spa.' + '<br>' + 'Where will you install your Echo Collection cement and\ ' +
-            'concrete tiles? (And when you do, please send pictures so we can add them here!)';
+            'and the Serengeti tile at the Terranea Resort and Spa.</p><p>Where will you install your Echo Collection cement and\ ' +
+            'concrete tiles? (And when you do, please send pictures so we can add them here!)</p>';
 
 
         /*gallerySvc.getGalleries().then(function (response){
