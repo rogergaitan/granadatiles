919b1ab8d6d2bb55067fd5c6aaed1d7cb13961a0
Erving Bonilla <ervingjbonilla@gmail.com>
2015-09-27 19:20:21 -0600

    link url news/books in  app news

--- granadatiles_project/apps/news/views.py
+++ granadatiles_project/apps/news/views.py
@@ -4,14 +4,18 @@ from core.views import BaseViewSet
 from .serializers import CatalogSerializer
 from .services import CatalogService
 
+
 def news(request):
     return render(request, 'news/article_list.html')
 
 
+def books(request):
+    return render(request, 'news/books_list.html')
+
+
 class CatalogViewSet(BaseViewSet):
-    
+
     def list(self, request):
-       catalogs = CatalogService.get_catalogs(language=self.get_language(request))
-       serializer = CatalogSerializer(catalogs, many=True)
-       return Response(serializer.data)
-   
+        catalogs = CatalogService.get_catalogs(language=self.get_language(request))
+        serializer = CatalogSerializer(catalogs, many=True)
+        return Response(serializer.data)
