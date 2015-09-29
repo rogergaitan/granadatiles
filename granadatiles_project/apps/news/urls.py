6060b169ae9b5a6c280d97ab34f97a9dab4b0ca1
roger <roger@localhost.localdomain>
2015-09-29 13:32:08 -0600

    update catalog endpoint url

--- granadatiles_project/apps/news/urls.py
+++ granadatiles_project/apps/news/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from .views import CatalogViewSet, ArticleViewSet
 
 router = DefaultRouter()
-router.register('catalogs', CatalogViewSet, base_name='catalogs')
+router.register('news/catalogs', CatalogViewSet, base_name='catalogs')
 router.register('news/articles', ArticleViewSet, base_name='years')
 router.register('news', ArticleViewSet, base_name='articles')
 
