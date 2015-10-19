87fb60ee0461c57013ae5e2b684c22b45cae17db
roger <roger@localhost.localdomain>
2015-10-19 10:00:37 -0600

    update quickbooks app

--- granadatiles_project/apps/quickbooks/admin.py
+++ granadatiles_project/apps/quickbooks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'quantity_on_hand')
+    list_filter = ('full_name',)
+    search_fields = ('full_name',)
