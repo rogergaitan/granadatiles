a63be75db065be621be13816210e78ceac1ba0de
LRPalacios <lrpalaciosdev@gmail.com>
2015-10-21 11:40:00 -0600

    add authentication by token

--- granadatiles_project/apps/quickbooks/models.py
+++ granadatiles_project/apps/quickbooks/models.py
@@ -1,5 +1,15 @@
-from django.db import models
+﻿from django.db import models
 from django.utils.translation import ugettext as _
+from django.dispatch.dispatcher import receiver
+from django.db.models.signals import post_save
+from django.conf import settings
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
 class Item(models.Model):
