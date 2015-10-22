727e377d69ff9e404ad07b98f98b0fca0b86b4cf
LRPalacios <lrpalaciosdev@gmail.com>
2015-10-22 13:57:09 -0600

    update command to test timeout

---
+++ granadatiles_project/apps/quickbooks/migrations/0002_auto_20151022_1333.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('quickbooks', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='item',
+            name='quantity_on_hand',
+            field=models.IntegerField(verbose_name='Quantity'),
+        ),
+    ]
