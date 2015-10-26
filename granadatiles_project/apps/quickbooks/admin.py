3cb9405787d28569a5c139ca3e646c90d6063004
roger <roger@localhost.localdomain>
2015-10-26 10:57:31 -0600

    delete quickbooks app

--- granadatiles_project/apps/quickbooks/admin.py
+++
@@ -1,8 +0,0 @@
-﻿from django.contrib import admin
-from .models import Item
-
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'full_name', 'quantity_on_hand','sales_price', 'sublevel', 'is_active')
-    list_filter = ('is_active', 'sublevel')
-    search_fields = ('full_name', 'name')
