190ccf93e3d42b5a3be13117df91d8b1c16cacff
roger <roger@localhost.localdomain>
2015-10-27 16:28:10 -0600

    fix previous

--- granadatiles_project/apps/tiles/models.py
+++ granadatiles_project/apps/tiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.utils.translation import ugettext as _
 from django.dispatch.dispatcher import receiver
 from django.conf import settings
