3ceba9605ed5a82fb6463a1e25464c4c89d78295
roger <roger@localhost.localdomain>
2015-11-16 15:41:30 -0600

    remove quickbooks app

--- granadatiles_project/apps/tiles/urls.py
+++ granadatiles_project/apps/tiles/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, url
 from . import views
 from rest_framework.routers import DefaultRouter
-from apps.tiles.views import CollectionViewSet, GroupViewSet, ItemsViewSet
+from apps.tiles.views import CollectionViewSet, GroupViewSet
 
 router = DefaultRouter()
 router.register('collections', CollectionViewSet, base_name='collections')
 router.register('groups', GroupViewSet, base_name='groups')
-router.register('quickbooks/items', ItemViewSet, base_name='items')
 
 urlpatterns = router.urls
