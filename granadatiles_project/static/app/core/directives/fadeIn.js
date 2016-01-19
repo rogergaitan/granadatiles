179546de8e0a65c7f447f9ff062c22dbf260caf0
LRPalacios <lrpalaciosdev@gmail.com>
2016-01-19 11:29:16 -0600

    add fade in effect when changing main

---
+++ granadatiles_project/static/app/core/directives/fadeIn.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .directive('fadeIn', fadeIn);
+
+    fadeIn.$inject = ['$timeout'];
+
+    function fadeIn($timeout) {
+
+        var directive = {
+            link: link,
+            restrict: 'A'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {
+            element.on("load", function () {
+                $timeout(function () {
+                    element.removeClass("ng-hide-fade");
+                    element.addClass("ng-show");
+                }, 100);
+            });
+            attrs.$observe("ngSrc", function () {
+                element.removeClass("ng-show");
+                element.addClass("ng-hide-fade");
+            });
+        }
+    }
+
+})();
--- granadatiles_project/static/app/tiles/controllers/collectionGroupsCtrl.js
+++ granadatiles_project/static/app/tiles/controllers/collectionGroupsCtrl.js
@@ -9,14 +9,16 @@
                      'collectionsSvc',
                      '$modal',
                      '$scope',
+                     '$timeout',
                      collectionsGroupCtrl
-        ]);
+                    ]);
 
     function collectionsGroupCtrl(baseSettings,
                                   pageSettings,
                                   collectionsSvc,
                                   $modal,
-                                  $scope) {
+                                  $scope,
+                                  $timeout) {
         var vm = this;
 
         vm.labels = pageSettings.labels;
@@ -36,7 +38,7 @@
         collectionsSvc.getFilteredMenuCollection(pageSettings.collectionId).then(function (response) {
             vm.filteredMenuCollection = response.data;
         });
-        if(pageSettings.groupId >= 0){
+        if (pageSettings.groupId >= 0) {
             collectionsSvc.getGroup(pageSettings.groupId).then(function (response) {
                 vm.group = response.data;
             });
@@ -53,7 +55,7 @@
             //vm.selectedSize = vm.sizes[0];
         });*/
 
-        if(pageSettings.groupId >= 0) {
+        if (pageSettings.groupId >= 0) {
             collectionsSvc.getStyles(pageSettings.groupId).then(function (response) {
                 vm.styles = response.data;
 
@@ -78,71 +80,74 @@
             });
         }
 
-        vm.setSize = function(size) {
+        vm.setSize = function (size) {
             vm.selectedSize = size;
         };
 
-        vm.setStyle = function(style) {
+        vm.setStyle = function (style) {
             vm.selectedStyle = style;
             updateTilesByStyle(style);
         };
 
-        vm.setTile = function(index, tileId){
-            collectionsSvc.getMainTile(tileId).then(function (response){
+        vm.setTile = function (index, tileId) {
+
+            collectionsSvc.getMainTile(tileId).then(function (response) {
+                var arrayTiles = [];
+                for (var i = 0; i < vm.tiles[index].tiles.length; i++) {
+                    if (vm.tiles[index].tiles[i].id != tileId) {
+                        arrayTiles.push(vm.tiles[index].tiles[i])
+                    }
+                }
+                arrayTiles.push({
+                    'id': vm.tiles[index].main.id,
+                    'name': vm.tiles[index].main.name,
+                    'image': vm.tiles[index].main.image,
+                    'sizes': vm.tiles[index].main.sizes,
+                    'hasInstallationPhotos': vm.tiles[index].main.hasInstallationPhotos,
+                    'hasSample': vm.tiles[index].main.hasSample
+                });
+                vm.tiles[index].tiles = arrayTiles;
                 vm.main = response.data;
+                vm.tiles[index].previousImage = vm.tiles[index].main.mosaic;
                 updateMain(index, vm.main);
             });
 
-            var arrayTiles = [];
-            for ( var i = 0; i < vm.tiles[index].tiles.length;i++ ){
-                if(vm.tiles[index].tiles[i].id != tileId){
-                    arrayTiles.push(vm.tiles[index].tiles[i])
-                }
-            }
-            arrayTiles.push({
-                'id':vm.tiles[index].main.id,
-                'name':vm.tiles[index].main.name,
-                'image':vm.tiles[index].main.image,
-                'sizes':vm.tiles[index].main.sizes,
-                'hasInstallationPhotos':vm.tiles[index].main.hasInstallationPhotos,
-                'hasSample':vm.tiles[index].main.hasSample
-            });
-            vm.tiles[index].tiles = arrayTiles;
-
         };
-        function updateMain(index, main){
+
+
+        function updateMain(index, main) {
             var setMain = [];
             setMain.push({
-                'id':main.id,
-                'name':main.name,
-                'mosaic':main.mosaic,
-                'image':main.image,
-                'sizes':main.sizes,
-                'hasInstallationPhotos':main.hasInstallationPhotos,
-                'hasSample':main.hasSample
+                'id': main.id,
+                'name': main.name,
+                'mosaic': main.mosaic,
+                'image': main.image,
+                'sizes': main.sizes,
+                'hasInstallationPhotos': main.hasInstallationPhotos,
+                'hasSample': main.hasSample
             });
             vm.tiles[index].main = setMain[0];
         }
 
-        function updateTilesByStyle(selectedStyle){
-            if(selectedStyle.id == 0){
+        function updateTilesByStyle(selectedStyle) {
+            if (selectedStyle.id == 0) {
                 collectionsSvc.getTiles(pageSettings.groupId).then(function (response) {
                     vm.tiles = response.data;
                 });
-            }else{
-                collectionsSvc.getTileFilteredByStyle(pageSettings.groupId, selectedStyle.name).then(function (response){
+            } else {
+                collectionsSvc.getTileFilteredByStyle(pageSettings.groupId, selectedStyle.name).then(function (response) {
                     vm.tiles = response.data;
                 });
             }
         }
 
-        vm.showInstallationPhoto = function(tileId) {
+        vm.showInstallationPhoto = function (tileId) {
             $modal.open({
                 templateUrl: baseSettings.staticUrl + 'app/tiles/templates/tileModal.html',
                 controller: 'tileModalCtrl',
                 controllerAs: 'vm',
-                size:'lg',
-                resolve:{
+                size: 'lg',
+                resolve: {
                     installationPhotos: function () {
                         return collectionsSvc.getInstallationPhoto(tileId).then(function (response) {
                             return response.data;
