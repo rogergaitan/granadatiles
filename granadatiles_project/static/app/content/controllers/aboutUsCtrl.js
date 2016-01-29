06e2c0ac1afd831a00e2f5f1bc1a4f1b14a0c6ae
LRPalacios <luisz54@hotmail.com>
2016-01-28 22:46:15 -0600

    add about us page

---
+++ granadatiles_project/static/app/content/controllers/aboutUsCtrl.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.content')
+        .controller('aboutUsCtrl', aboutUsCtrl);
+
+    aboutUsCtrl.$inject = ['pageSettings', 'sectionSvc'];
+
+    function aboutUsCtrl(pageSettings, sectionSvc) {
+        /* jshint validthis:true */
+        var vm = this;
+       
+        sectionSvc.getSection(pageSettings.sectionId).then(function (response) {
+            vm.section = response.data;
+        });
+    }
+})();
--- granadatiles_project/static/app/content/controllers/contentDefaultCtrl.js
+++ granadatiles_project/static/app/content/controllers/contentDefaultCtrl.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app')
+        .module('app.content')
         .controller('contentDefaultCtrl',
             ['baseSettings',
              'pageSettings',
