f666dff729c6aa1d0518f22d64d7edbe9a9d2572
LRPalacios <luisz54@hotmail.com>
2016-02-02 11:56:30 -0600

    add forms to handle login, register, recover password

--- granadatiles_project/static/app/portfolio/controller/portfolioLoginCtrl.js
+++ granadatiles_project/static/app/portfolio/controller/portfolioLoginCtrl.js
@@ -5,12 +5,25 @@
         .module('app.portfolio')
         .controller('portfolioLoginCtrl', portfolioLoginCtrl);
 
-    portfolioLoginCtrl.$inject = ['pageSettings'];
+    portfolioLoginCtrl.$inject = ['pageSettings', 'appSettings'];
 
-    function portfolioLoginCtrl(pageSettings) {
+    function portfolioLoginCtrl(pageSettings, appSettings) {
         /* jshint validthis:true */
         var vm = this;
         vm.labels = pageSettings.labels;
 
+        vm.activeTemplateUrl = appSettings.staticUrl + 'app/portfolio/templates/login/login.html';
+
+        vm.goToForgotPassword = function () {
+            vm.activeTemplateUrl = appSettings.staticUrl + 'app/portfolio/templates/login/forgotPassword.html';
+        }
+
+        vm.goToLogIn = function () {
+            vm.activeTemplateUrl = appSettings.staticUrl + 'app/portfolio/templates/login/login.html';
+        }
+
+        vm.goToSignUp = function () {
+            vm.activeTemplateUrl = appSettings.staticUrl + 'app/portfolio/templates/login/signUp.html';
+        }
     }
 })();
