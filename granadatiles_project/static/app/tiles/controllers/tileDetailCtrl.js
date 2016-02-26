c5ddec22523e8bf49327f1b51076988dc9964979
LRPalacios <lrpalaciosdev@gmail.com>
2016-02-26 16:48:27 -0600

    add service to calculate quantities

--- granadatiles_project/static/app/tiles/controllers/tileDetailCtrl.js
+++ granadatiles_project/static/app/tiles/controllers/tileDetailCtrl.js
@@ -7,21 +7,28 @@
 
     tileDetailCtrl.$inject = ['$scope',
                               'tilesSvc',
+                              'tilesLogicSvc',
                               'pageSettings',
                               'portfolioSvc',
                               '$modal',
                               'baseSettings',
                               'collectionsSvc'];
 
-    function tileDetailCtrl($scope, tilesSvc, pageSettings, portfolioSvc, $modal, baseSettings, collectionsSvc) {
+    function tileDetailCtrl($scope, tilesSvc, tilesLogicSvc, pageSettings, portfolioSvc, $modal, baseSettings, collectionsSvc) {
         var selectedTileId = $scope.shared.tileId;
 
         var vm = this;
         vm.labels = pageSettings.labels;
+        vm.selectedWarehouse = vm.labels.shipFrom;
         vm.collection = $scope.shared.collection;
         vm.group = $scope.shared.group;
         vm.isAuthenticated = baseSettings.userIsAuthenticated;
         vm.navigation = baseSettings.navigation;
+        vm.nortonImage = baseSettings.staticUrl + 'img/Norton-seal.png';
+        vm.Order = {
+            inputSqFt: 0,
+            shipFromWarehouseId:0
+        }
 
         vm.backToGroup = function () {
             $scope.shared.tileDetailTemplateUrl = '';
@@ -29,6 +36,8 @@
 
         tilesSvc.getTileDetail(selectedTileId).then(function (response){
             vm.tile = response.data;
+            vm.tile.quantityPerBox = tilesLogicSvc.calculateQuantityInBox(vm.tile);
+            vm.tile.pricePerBox = tilesLogicSvc.calculatePricePerBox(vm.tile)
             if(vm.tile.sizes.length > 0){
                 vm.selectedSize = vm.tile.sizes[0].size
             }
@@ -60,6 +69,10 @@
         vm.setSize = function (size) {
             vm.selectedSize = size.size;
         };
+
+        vm.setWarehouse = function (warehouse) {
+            vm.selectedWarehouse = warehouse.name;
+        }
 
         vm.printPage = function () {
             print();
---
+++ granadatiles_project/static/app/tiles/services/tilesLogicSvc.js
@@ -0,0 +1,45 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('tilesLogicSvc', tilesLogicSvc);
+
+
+    function tilesLogicSvc() {
+        var service = {
+            calculateQuantityInBox: calculateQuantityInBox,
+            calculatePricePerBox: calculatePricePerBox
+        };
+
+        return service;
+
+        function calculateQuantityInBox(tile) {
+            if (tile.box.measurementUnit == 1 && !tile.qtyIsSqFt) {
+                return tile.quantity/tile.box.quantity
+            }
+
+            if (tile.box.measurementUnit == 2 && tile.qtyIsSqFt) {
+                return tile.quantity/tile.box.quantity
+            }
+
+            if (tile.box.measurementUnit == 1 && tile.qtyIsSqFt) {
+                return (tile.quantity * tile.sqFt) / tile.box.quantity
+            }
+        }
+
+        function calculatePricePerBox(tile) {
+            if (tile.box.measurementUnit == 1 && !tile.qtyIsSqFt) {
+                return tile.price * tile.box.quantity
+            }
+
+            if (tile.box.measurementUnit == 2 && tile.qtyIsSqFt) {
+                return tile.price * tile.box.quantity
+            }
+
+            if (tile.box.measurementUnit == 1 && tile.qtyIsSqFt) {
+                return (tile.quantity * tile.sqFt) * tile.box.quantity
+            }
+        }
+    }
+})();
