4cc25481c5798e0b01a5f2ad718c66ceb32fcd3e
LRPalacios <luisz54@hotmail.com>
2016-03-26 18:17:30 -0600

    add customizedTiles in checkout

--- granadatiles_project/static/app/cart/controllers/checkoutCtrl.js
+++ granadatiles_project/static/app/cart/controllers/checkoutCtrl.js
@@ -5,9 +5,9 @@
         .module('app.cart')
         .controller('checkoutCtrl', checkoutCtrl);
 
-    checkoutCtrl.$inject = ['pageSettings', 'cartSvc', 'checkoutSvc', '$q'];
+    checkoutCtrl.$inject = ['pageSettings', 'cartSvc', 'checkoutSvc', 'customTilesSvc', '$q'];
 
-    function checkoutCtrl(pageSettings, cartSvc, checkoutSvc, $q) {
+    function checkoutCtrl(pageSettings, cartSvc, checkoutSvc, customTilesSvc, $q) {
         /* jshint validthis:true */
         var vm = this;
         vm.labels = pageSettings.labels;
@@ -18,12 +18,20 @@
 
         var orders = cartSvc.getCartTiles();
         var sampleOrders = cartSvc.getCartSamples();
+        var cartCustomizedTilesOrders = cartSvc.getCustomizedTiles();
 
-        $q.all([orders, sampleOrders]).then(function (response) {
+        $q.all([orders, sampleOrders, cartCustomizedTilesOrders]).then(function (response) {
             vm.orders = response[0].data;
             vm.sampleOrders = response[1].data;
+            vm.customizedOrders = response[2].data;
+
             for (var i = 0; i < vm.orders.length; i++) {
                 vm.subTotal += vm.orders[i].subtotal;
+            }
+            for (var i = 0; i < vm.customizedOrders.length; i++) {
+                vm.customizedOrders[i].tile.colors = customTilesSvc.getColorsUsed(vm.customizedOrders[i].groupColors);
+                vm.customizedOrders[i].tile.inStock = false;
+                vm.subTotal += vm.customizedOrders[i].subtotal;
             }
             for (var i = 0; i < vm.sampleOrders.length; i++) {
                 vm.subTotal += vm.sampleOrders[i].subtotal;
