f0d1c07665675386a402b1c0872f5044bf02790c
roger <roger@localhost.localdomain>
2016-04-18 16:05:51 -0600

    add order create endpoint

---
+++ granadatiles_project/apps/orders/services.py
@@ -0,0 +1,78 @@
+from django.contrib.auth.models import User
+
+from .models import Order, OrderDetail, Address
+from apps.tiles.models import Tile, CustomizedTile
+
+
+class OrdersService:
+    
+    def save_tile_order(customer_data, shipping_address, billing_address, tiles, customized_tiles):
+        try:
+            user = User.objects.get(pk=customer_data['user'])
+        except User.DoesNotExist:
+            user = None
+        
+        order = Order.objects.create(
+            customer_first_name=customer_data['customerFirstName'],
+            customer_last_name=customer_data['customerLastName'],
+            customer_company_name=customer_data['customerCompanyName'],
+            credit_card_number=customer_data['creditCardNumber'],
+            ip_address=customer_data['ipAddress'],
+            user=user,
+            same_as_shipping=customer_data['sameAsShipping']
+        )
+        
+        Address.objects.create(
+            order=order,
+            street_building_house_line1=shipping_address['streetBuildingHouseLine1'],
+            house_apt_line2=shipping_address['houseAptLine2'],
+            zipcode=shipping_address['zipcode'],
+            province_state=shipping_address['provinceState'],
+            city=shipping_address['city'],
+            type=2            
+        )
+        
+        if customer_data['sameAsShipping'] is not False:
+            Address.objects.create(
+                order=order,
+                street_building_house_line1=billing_address['streetBuildingHouseLine1'],
+                house_apt_line2=billing_address['houseAptLine2'],
+                zipcode=billing_address['zipcode'],
+                province_state=billing_address['provinceState'],
+                city=billing_address['city'],
+                type=1
+            )
+        
+        for tile_detail in tiles:
+            try:
+                tile = Tile.objects.get(pk=tile_detail.id)
+            except Tile.DoesNotExist:
+                tile = None
+            
+            OrderDetail.objects.create(
+                order=order,
+                tile=tile
+                customized_tile=None,
+                input_sq_ft=tile_detail['inputSqFt'],
+                price_per_sq_feet=tile_detail['pricePerSqFeet'],
+                price_per_tile=tile_detail['pricePerTile'],
+                base_cost=tile_detail['baseCost'],
+                box=tile.box
+            )
+            
+        for customized_tile_detail in customized_tiles:
+            try:
+                customized_tile = CustomizedTile.objects.get(pk=customized_tile_detail.id)
+            except CustomizedTile.DoesNotExist:
+                customized_tile = None
+                
+            OrderDetail.objects.create(
+                order=order,
+                tile=None
+                customized_tile=customized_tile,
+                input_sq_ft=tile_detail['inputSqFt'],
+                price_per_sq_feet=tile_detail['pricePerSqFeet'],
+                price_per_tile=tile_detail['pricePerTile'],
+                base_cost=tile_detail['baseCost'],
+                box=customized_tile.box
+            )
--- granadatiles_project/apps/orders/views.py
+++ granadatiles_project/apps/orders/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import list_route, detail_route
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-# Create your views here.
+from core.views import BaseViewSet
+from .services import OrdersService
+
+
+class OrdersViewSet(BaseViewSet):
+    
+    def create(self, request):
+        customer_data = request.data.get('customerData')
+        shipping_address = request.data.get('shippingAddress')
+        billing_address = request.data.get('billingAddress')
+        tiles = request.data.get('tiles')
+        customized_tiles = request.data.get('customizedTiles')
+        return Response(OrdersService.save_order(customer_data, shipping_address, 
+                                                 billing_address, tiles, customized_tiles))
