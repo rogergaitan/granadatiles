432a8db161dd1c23ad0d7a85dd9ec34a6f7b6164
roger <roger@localhost.localdomain>
2016-04-27 15:20:54 -0600

    tile admin add is not square field

--- granadatiles_project/apps/tiles/admin.py
+++ granadatiles_project/apps/tiles/admin.py
@@ -116,7 +116,7 @@ class TileAdmin(admin.ModelAdmin):
               'sales_description_es', 'size', 'height', 'width' ,'thickness','weight',
               'sales_price','average_cost', 'quantity_on_hand','image','rotate_deg1', 
               'rotate_deg2', 'rotate_deg3', 'rotate_deg4', 'plane',
-              'tearsheet', 'box',  'similar_tiles', 'main', 'new', 'in_stock',
+              'tearsheet', 'box',  'similar_tiles', 'main', 'new', 'in_stock', 'is_not_square',
               'is_sample', 'sample', 'override_collection_box', 'is_active', 'on_sale', 'import_colors')
 
     list_display = ('name', 'sales_description', 'size', 'weight', 'thickness',
